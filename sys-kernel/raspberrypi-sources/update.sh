b19d5c0ad05eeeb4717c903d0910f7499f436529
Gary Macindoe <gary@garymacindoe.co.uk>
2015-11-06 19:46:28 +0000

    Rewrite update script to parse release commits from firmware repo

--- sys-kernel/raspberrypi-sources/update.sh
+++ sys-kernel/raspberrypi-sources/update.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 
-for sublevel in 0 1 2 3
+git -C ${HOME}/git/firmware log --pretty=format:"%s %h" -- extra/git_hash | grep "kernel: Bump to [0-9].[0-9]*.[0-9]*" | cut -d' ' -f4,5 | while read version commit
   do
-  git -C ${HOME}/git/linux log --pretty=format:"%s %h" origin/rpi-4.${sublevel}.y | grep "^Linux 4.${sublevel}[.0-9]* " | while read linux version hash
-    do
-    sed -e "s/ versionator$//" -e "s/^EGIT_BRANCH=.*$/EGIT_BRANCH=\"rpi-4.${sublevel}.y\"\nEGIT_COMMIT=\"${hash}\"/" -e "s/^KEYWORDS=\"\"$/KEYWORDS=\"~arm\"/" ${HOME}/git/gentoo-overlay/sys-kernel/raspberrypi-sources/raspberrypi-sources-4.${sublevel}.9999.ebuild > ${HOME}/git/gentoo-overlay/sys-kernel/raspberrypi-sources/raspberrypi-sources-${version}.ebuild
-  done
-  mv ${HOME}/git/gentoo-overlay/sys-kernel/raspberrypi-sources/raspberrypi-sources-4.${sublevel}.ebuild ${HOME}/git/gentoo-overlay/sys-kernel/raspberrypi-sources/raspberrypi-sources-4.${sublevel}.0.ebuild
+  v=(${version//./ })
+  h=$(git -C ${HOME}/git/firmware show ${commit}:extra/git_hash | cut -c 1-7)
+  sed -e "s/ versionator$//" -e "s/^EGIT_BRANCH=.*$/EGIT_BRANCH=\"rpi-${v[0]}.${v[1]}.y\"\nEGIT_COMMIT=\"${h}\"/" -e "s/^KEYWORDS=\"\"$/KEYWORDS=\"~arm\"/" ${HOME}/git/gentoo-overlay/sys-kernel/raspberrypi-sources/raspberrypi-sources-${v[0]}.${v[1]}.9999.ebuild > ${HOME}/git/gentoo-overlay/sys-kernel/raspberrypi-sources/raspberrypi-sources-${v[0]}.${v[1]}.${v[2]}.ebuild
 done
